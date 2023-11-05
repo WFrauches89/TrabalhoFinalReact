d09a8256d18eeca8b80634a7015e8130419939ce
Wanderson Frauches <wfrauches89@gmail.com>
2023-11-05 15:05:54 -0300

    Atualizando code home

---
+++ TrabalhoFinalReact/src/App.jsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import Navigation from "./Navigation/Nav";
+import Products from "./Products/products";
+import Recommended from "./Recommended/Recommended";
+import Sidebar from "./Sidebar/Sidebar";
+//Database
+import products from "./db/data";
+import Card from "./components/Card";
+import "./index.css"
+
+function App() {
+
+  //Placing all of the state inside this app component
+  const [selectedCategory, setSelectedCategory] = useState(null)
+  //Input Filter
+  const [query, setQuery] = useState("")
+
+  //Capture Filter
+  const handleInputChange = (e) => {
+    setQuery(e.target.value)
+  }
+
+  //
+  const filteredItems = products.filter((product) => product.title
+    .toLocaleLowerCase()
+    .indexOf(query.toLocaleLowerCase()) !== -1
+  );
+
+  // Radio Filter - User Selection Radio ----------------------------------------------------------------
+  const handleChange = (e) => {
+    setSelectedCategory(e.target.value)
+  }
+
+  // Buttons Filter 
+  const handleClick = (e) => {
+    setSelectedCategory(e.target.value)
+  }
+
+  function filteredData(products, selected, query) {
+    let filteredProducts = products
+
+    if (query) {
+      filteredProducts = filteredItems
+    }
+
+    //Selected Itens
+    if (selected) {
+      filteredProducts = filteredProducts.filter(({ category, color, company, newPrice, title }) => 
+      category === selected ||
+      color === selected ||
+      company === selected ||
+      newPrice === selected ||
+      title === selected);
+    }
+
+    return filteredProducts.map(
+      ({img, title, star,reviews, newPrice, prevPrice}) => (
+      <Card
+       key={Math.random()}
+       img={img}
+       title={title}
+       star={star}
+       reviews={reviews}
+       newPrice={newPrice}
+       prevPrice={prevPrice}  
+       />
+    ));
+  }
+
+    const result = filteredData(products, selectedCategory, query);
+
+
+
+    return (
+      <>
+        <Sidebar handleChange={handleChange}/>
+        <Navigation query={query} handleInputChange={handleInputChange}/>
+        <Recommended handleClick={handleClick}/>
+        <Products result={result}/>
+      </>
+    );
+  }
+
+  export default App
